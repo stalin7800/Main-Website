38b31d7490624b8041e9b4fd68f73042c80b4f8e
karate7800 <karate7800@gmail.com>
2022-07-09 22:21:33 -0400

    remodify svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
-import adapter from '@sveltejs/adapter-netlify';
-
 export default {
   kit: {
-    adapter: adapter()
+    // default options are shown
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
   }
 };
