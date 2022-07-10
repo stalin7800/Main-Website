7a3f8cdc53332e96de903df0a911cd1353783591
karate7800 <karate7800@gmail.com>
2022-07-09 22:45:34 -0400

    update app

---
+++ .svelte-kit/output/server/nodes/2.js
@@ -0,0 +1,7 @@
+import * as module from '../entries/pages/about/index.svelte.js';
+
+export { module };
+export const index = 2;
+export const entry = 'pages/about/index.svelte-cf83a470.js';
+export const js = ["pages/about/index.svelte-cf83a470.js","chunks/index-00f759cf.js"];
+export const css = ["assets/pages/about/index.svelte-fa522dc4.css"];
